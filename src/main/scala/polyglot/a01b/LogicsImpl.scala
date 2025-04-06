c2fab697388eee5a3a6b3ec62bed23dda43ac5c6
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-04-06 11:54:10 +0200

    impl LogicsImpl

--- src/main/scala/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/polyglot/a01b/LogicsImpl.scala
@@ -2,7 +2,8 @@ package polyglot.a01b
 
 import polyglot.OptionToOptional
 import util.Optionals.Optional as ScalaOptional
-import polyglot.a01b.Logics
+import util.Sequences.Sequence
+import util.Streams.Stream
 
 import scala.jdk.javaapi.OptionConverters
 
@@ -12,7 +13,25 @@ trait Logics:
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
-  def hit(x: Int, y: Int): java.util.Optional[Integer] =
-    OptionToOptional(ScalaOptional.Empty()) // Option => Optional converter
+  private case class pair(x: Int, y: Int)
+  private var minesSet: Sequence[pair] = Sequence.Nil()
+  private var selected: Sequence[pair] = Sequence.Nil()
+  private val random = scala.util.Random()
+  while (minesSet.size() != size)
+    minesSet = Sequence.Cons(pair(random.nextInt(size), random.nextInt(size)), minesSet)
+  println(minesSet)
 
-  def won = false
+  private def neighbours(x: Int, y: Int): Int = Stream.iterate(x - 1)(_ + 2)
+    .flatMap(xx => Stream.iterate(y - 1)(_ + 2).map(yy => pair(xx, yy)))
+    .toList
+    .filter(minesSet.contains(_))
+    .size()
+
+  override def hit(x: Int, y: Int): java.util.Optional[Integer] =
+    val p = pair(x, y)
+    OptionToOptional(if minesSet.contains(p) then ScalaOptional.Empty() else
+      selected = Sequence.Cons(p, selected)
+      ScalaOptional.Just(neighbours(x, y))
+    )
+
+  override def won(): Boolean = selected.size() + minesSet.size() == size * size
