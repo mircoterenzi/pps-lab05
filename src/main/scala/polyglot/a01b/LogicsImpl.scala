35ab21936b85c6fe8257cbb2d6d8d98ff6f2b436
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-04-06 11:53:58 +0200

    add trait Logics

--- src/main/scala/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/polyglot/a01b/LogicsImpl.scala
@@ -6,6 +6,10 @@ import polyglot.a01b.Logics
 
 import scala.jdk.javaapi.OptionConverters
 
+trait Logics:
+  def hit(x: Int, y: Int): java.util.Optional[Integer]
+  def won(): Boolean
+
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
   def hit(x: Int, y: Int): java.util.Optional[Integer] =
