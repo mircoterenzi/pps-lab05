bd1abe11e52534f9aa80252ec827160675b74d1d
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-04-06 14:30:33 +0200

    impl of SchoolModel Scala OOP

--- src/main/scala/ex/Ex2SchoolModel.scala
+++ src/main/scala/ex/Ex2SchoolModel.scala
@@ -3,74 +3,65 @@ package ex
 import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.Test
 import util.Sequences.Sequence
-import util.Sequences.Sequence.*
+import Sequence.*
+import ex.SchoolModel.*
+import BasicSchool.*
 
 object SchoolModel:
+  type School = SchoolModule
+  type Teacher = String
+  type Course = String
 
   trait SchoolModule:
-    type School
-    type Teacher
-    type Course
+    def courses(): Sequence[Course]
+    def teachers(): Sequence[Teacher]
+    def setTeacherToCourse(teacher: Teacher, course: Course): School
+    def coursesOfATeacher(teacher: Teacher): Sequence[Course]
+    def hasTeacher(name: String): Boolean
+    def hasCourse(name: String): Boolean
 
-    def teacher(name: String): Teacher
-    def course(name: String): Course
-    def emptySchool: School
-    extension (school: School)
-      def courses(): Sequence[Course]
-      def teachers(): Sequence[Teacher]
-      def setTeacherToCourse(teacher: Teacher, course: Course): School
-      def coursesOfATeacher(teacher: Teacher): Sequence[Course]
-      def hasTeacher(name: String): Boolean
-      def hasCourse(name: String): Boolean
+  private case class TeacherToCourse(t: Teacher, c: Course)
 
-  object BasicSchoolModule extends SchoolModule:
-    case class TeacherToCourse(t: Teacher, c: Course)
+  private class BasicSchoolModule(private var school: Sequence[TeacherToCourse] = Nil()) extends SchoolModule:
+    def courses(): Sequence[String] = school.map {
+      case TeacherToCourse(t, c) => c
+    }.distinct()
+    def teachers(): Sequence[String] = school.map {
+      case TeacherToCourse(t, c) => t
+    }.distinct()
+    def setTeacherToCourse(teacher: Teacher, course: Course): School =
+      BasicSchoolModule(Cons(TeacherToCourse(teacher, course), school))
+    def coursesOfATeacher(teacher: Teacher): Sequence[Course] =
+      school.flatMap {
+        case TeacherToCourse(t, c) if t == teacher => Cons(c, Nil())
+        case _ => Nil()
+      }
+    def hasTeacher(name: String): Boolean = teachers().contains(name)
+    def hasCourse(name: String): Boolean = courses().contains(name)
 
-    override type School = Sequence[TeacherToCourse]
-    override type Teacher = String
-    override type Course = String
-
+  object BasicSchool:
+    def emptySchool: School = new BasicSchoolModule()
     def teacher(name: String): Teacher = name
     def course(name: String): Course = name
-    def emptySchool: School = Nil()
-
-    extension (school: School)
-      def courses(): Sequence[String] = school.map {
-        case TeacherToCourse(t, c) => c
-      }.distinct()
-      def teachers(): Sequence[String] = school.map {
-        case TeacherToCourse(t, c) => t
-      }.distinct()
-      def setTeacherToCourse(teacher: Teacher, course: Course): School =
-        school.concat(Cons(TeacherToCourse(teacher, course), Nil()))
-      def coursesOfATeacher(teacher: Teacher): Sequence[Course] =
-        school.flatMap {
-          case TeacherToCourse(t, c) if t == teacher => Cons(c, Nil())
-          case _ => Nil()
-        }
-      def hasTeacher(name: String): Boolean = teachers().contains(name)
-      def hasCourse(name: String): Boolean = courses().contains(name)
-
 
 class SchoolModelTest:
-  import SchoolModel.BasicSchoolModule.*
-  val school: School = emptySchool
-  val john: Teacher = teacher("John")
-  val pablo: Teacher = teacher("Pablo")
-  val math: Course = course("Math")
-  val italian: Course = course("Italian")
-  val school2: School = school.setTeacherToCourse(john, math)
-  val school3: School = school2.setTeacherToCourse(john, italian).setTeacherToCourse(pablo, italian)
+  private val school = emptySchool
+  private val john: Teacher = teacher("John")
+  private val pablo: Teacher = teacher("Pablo")
+  private val math: Course = course("Math")
+  private val italian: Course = course("Italian")
+  private val school2: School = school.setTeacherToCourse(john, math)
+  private val school3: School = school2.setTeacherToCourse(john, italian).setTeacherToCourse(pablo, italian)
 
   @Test def testTeacher(): Unit =
     assertEquals(Nil(), school.teachers())
     assertEquals(Cons("John", Nil()), school2.teachers())
-    assertEquals(Cons("John", Cons("Pablo", Nil())), school3.teachers())
+    assertEquals(Cons("Pablo", Cons("John", Nil())), school3.teachers())
 
   @Test def testCourses(): Unit =
     assertEquals(Nil(), school.courses())
     assertEquals(Cons("Math", Nil()), school2.courses())
-    assertEquals(Cons("Math", Cons("Italian", Nil())), school3.courses())
+    assertEquals(Cons("Italian", Cons("Math", Nil())), school3.courses())
 
   @Test def testHasTeacher(): Unit =
     assertFalse(school.hasTeacher("John"))
@@ -87,4 +78,4 @@ class SchoolModelTest:
   @Test def testCoursesOfATeacher(): Unit =
     assertEquals(Nil(), school.coursesOfATeacher(john))
     assertEquals(Cons("Math", Nil()), school2.coursesOfATeacher(john))
-    assertEquals(Cons("Math", Cons("Italian", Nil())), school3.coursesOfATeacher(john))
+    assertEquals(Cons("Italian", Cons("Math", Nil())), school3.coursesOfATeacher(john))
